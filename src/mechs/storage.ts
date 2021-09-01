293ab7ca01a85b65b21b780249fc823fdff18b6a
sdaliyot <shahar.daliyot@gmail.com>
2021-09-01 13:59:41 +0300

    Store original value on key

--- src/mechs/storage.ts
+++ src/mechs/storage.ts
@@ -5,6 +5,16 @@ const keyStorage = new WeakMap<core.CryptoKey, InternalCryptoKey>();
 
 export function getCryptoKey(key: core.CryptoKey) {
   const res = keyStorage.get(key);
+  // The key may not be found in the imported keys (after process restart) so we reconstruct (and import) it from the 
+  // source __value we stored on the (persisted) key
+  if (!res && key.__value) {
+    keyStorage.set(key, key.__value);
+    res = keyStorage.get(key);
+  }
+  // In some cases the algorithm is missing from the value but exists on the key so we take it from there
+  if (res && !res.algorithm && key.algorithm) {
+    res.algorithm = key.algorithm;
+  }
   if (!res) {
     throw new core.OperationError("Cannot get CryptoKey from secure storage");
   }
@@ -13,9 +23,9 @@ export function getCryptoKey(key: core.CryptoKey) {
 
 export function setCryptoKey(value: InternalCryptoKey) {
   const key = core.CryptoKey.create(value.algorithm, value.type, value.extractable, value.usages);
+  // Store the source value on the key (which is persisted) so we can reconstruct the original imported key after process restart.
+  key.__value = value;
   Object.freeze(key);
-
   keyStorage.set(key, value);
-
   return key;
 }
